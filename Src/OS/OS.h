c256cbf3671ead62215353604939e1d5ab29edad
Iliass Mahjoub <48915588+imahjoub@users.noreply.github.com>
2023-08-24 10:28:50 +0200

    Update OS.h

--- Src/OS/OS.h
+++ Src/OS/OS.h
@@ -1,24 +1,10 @@
 #ifndef OS_2023_08_23_H
   #define OS_2023_08_23_H
 
-  #include <stdint.h>
-
-
   #if defined(__cplusplus)
   extern "C"
   {
   #endif
-
-  typedef uint64_t OsTimerType;
-
-  typedef struct TCB
-  {
-    void(*pInit)(void);
-    void(*pFunc)(void);
-    OsTimerType CallTimeNext;
-    const unsigned CallCycle;
-  }
-  TCB;
 
   void OS_Start(void);
 
