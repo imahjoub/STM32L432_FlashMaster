7debba190412aa0b3124a037985bf404f49276cb
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-08-27 14:04:33 +0200

    clean up code

--- Src/Cdd/CddExtFlash/CddExtFlash.c
+++ Src/Cdd/CddExtFlash/CddExtFlash.c
@@ -1,45 +1,47 @@
 #include <Cdd/CddExtFlash/CddExtFlash.h>
 
+/* Define IS25LP128F commands */
+#define CMD_WRITE_ENABLE  0x06U
+#define CMD_PAGE_PROGRAM  0x02U
 
-// Function to enable writing
+/* Function to enable writing */
 void CddExtFlashWriteEnable(void)
 {
-  // Send Write Enable command
+  /* Send Write Enable command */
   CddSpiTransfer(CMD_WRITE_ENABLE);
 }
 
-// Function to wait until the chip is not busy
+/* Function to wait until the chip is not busy */
 void CddExtFlashWaitForRead(void)
 {
-  while (CddSpiTransfer(0x05U) & 0x01U); // Poll Status Register until busy bit is cleared
+  /* Poll Status Register until busy bit is cleared */
+  while (CddSpiTransfer(0x05U) & 0x01U);
 }
 
-// Function to write data to a sector
-void CddExtFlashWriteSector(uint32_t sector_address, uint8_t *data, uint32_t len)
+/* Function to write data to a sector */
+void CddExtFlashWriteSector(uint32_t SectorAddress, uint8_t *DataPtr, uint32_t DataLen)
 {
-  // Enable writing
+  /* Enable writing */
   CddExtFlashWriteEnable();
 
-  // Send Page Program command
+  /* Send Page Program command */
   CddSpiTransfer(CMD_PAGE_PROGRAM);
 
-  CddSpiTransfer((sector_address >> 16) & 0xFF);
+  CddSpiTransfer((SectorAddress >> 16U) & 0xFFU);
 
-  // Address byte 2
-  CddSpiTransfer((sector_address >> 8) & 0xFF);
+  /* Address byte 2 */
+  CddSpiTransfer((SectorAddress >>  8U) & 0xFFU);
 
-  // Address byte 1
-  CddSpiTransfer(sector_address & 0xFF);
+  /* Address byte 1 */
+  CddSpiTransfer((SectorAddress >>  0U) & 0xFFU);
 
-  // Address byte 0
-  // Write data
-  for (uint32_t i = 0; i < len; i++)
+  /* Address byte 0 */
+  /* Write data     */
+  for (uint32_t i = 0U; i < DataLen; ++i)
   {
-    CddSpiTransfer(data[i]);
+    CddSpiTransfer(DataPtr[i]);
   }
 
-  // Wait for completion
+  /* Wait for completion */
   CddExtFlashWaitForRead();
 }
-
-
--- Src/Cdd/CddExtFlash/CddExtFlash.h
+++ Src/Cdd/CddExtFlash/CddExtFlash.h
@@ -3,12 +3,8 @@
 
   #include <Cdd/CddSpi/CddSpi.h>
 
-  // Define IS25LP128F commands
-  #define CMD_WRITE_ENABLE  0x06
-  #define CMD_PAGE_PROGRAM  0x02
-
   void CddExtFlashWriteEnable(void);
   void CddExtFlashWaitForRead(void);
-  void CddExtFlashWriteSector(uint32_t sector_address, uint8_t *data, uint32_t len);
+  void CddExtFlashWriteSector(uint32_t SectorAddress, uint8_t *DataPtr, uint32_t DataLen);
 
 #endif // CDD_EXT_FLASH_2023_08_22_H
