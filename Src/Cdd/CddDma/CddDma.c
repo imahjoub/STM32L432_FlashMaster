49c07643cd1b6e03ccd8e41a98537d38e2c57321
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-08-27 17:01:33 +0200

    first implementation of DmaSpi_initialization

--- Src/Cdd/CddDma/CddDma.c
+++ Src/Cdd/CddDma/CddDma.c
@@ -7,16 +7,80 @@ void DMA2_Stream3_IRQHandler(void);
 
 void Dma2Stream3SpiTxInit(void)
 {
+  /*-------------------- DMA configuration --------------------*/
   /* Enable the peripheral clock for DMA2 */
- RCC_AHB1ENR |= (1UL<<22U);
+ RCC_AHB1ENR |= (uint32_t)(1UL << 22U);
 
+  /* Disable the DMA2 stream for the configuration */
+  DMA2_Stream3->CR = (uint32_t)(0UL << 0U);
 
+  // Wait until the stream is disable */
+  while((DMA2_Stream3->CR & ((uint32_t)(1UL << 0U)))) { }
+
+  /*------------------ DMA stream3 parameters ------------------*/
+  /* Enable DMA2 transfer error interrupt */
+  DMA2_Stream3->CR |= (uint32_t)(1UL << 2U);
+
+  /* Enable DMA2 transfer complete interrupt */
+  DMA2_Stream3->CR |= (uint32_t)(1UL << 4U);
+
+  /* Set DMA2 data transfer direction (mem to peripheral) */
+  DMA2_Stream3->CR |= (uint32_t)(1UL << 6U);
+
+  /* Set DMA2 memory increment mode */
+  DMA2_Stream3->CR |= (uint32_t)(1UL << 10U);
+
+  /* Select channel 3 used for SPI1 */
+  DMA2_Stream3->CR |= (uint32_t)(3UL << 25U);
+
+  /* Disable DMA2 direct mode disable */
+  DMA2_Stream3->FCR |= (uint32_t)(1UL << 2U);
+
+  /* FIFO threshold selection: full FIFO */
+  DMA2_Stream3->FCR |= (uint32_t)(3UL << 0U);
+
+  /* TBD Add MSize */
+
+  /* TBD Enable DMA interrupt trough NVIC */
 }
 
 void Dma2Stream2SpiRxInit(void)
 {
+  /*-------------------- DMA configuration --------------------*/
   /* Enable the peripheral clock for DMA2 */
- RCC_AHB1ENR |= (1UL<<22U);
+ RCC_AHB1ENR |= (uint32_t)(1UL << 22U);
+
+  /* Disable the DMA2 stream for the configuration */
+  DMA2_Stream2->CR = (uint32_t)(0UL << 0U);
+
+  // Wait until the stream is disable */
+  while((DMA2_Stream2->CR & ((uint32_t)(1UL << 0U)))) { }
+
+  /*------------------ DMA stream3 parameters ------------------*/
+  /* Enable DMA2 transfer error interrupt */
+  DMA2_Stream2->CR |= (uint32_t)(1UL << 2U);
+
+  /* Enable DMA2 transfer complete interrupt */
+  DMA2_Stream2->CR |= (uint32_t)(1UL << 4U);
+
+  /* Set DMA2 data transfer direction (peripheral to mem) */
+  DMA2_Stream2->CR &= (uint32_t)(~(1UL << 6U) | ~(1UL << 7U));
+
+  /* Set DMA2 memory increment mode */
+  DMA2_Stream2->CR |= (uint32_t)(1UL << 10U);
+
+  /* Select channel 3 used for SPI1 */
+  DMA2_Stream2->CR |= (uint32_t)(3UL << 25U);
+
+  /* Disable DMA2 direct mode disable */
+  DMA2_Stream2->FCR |= (uint32_t)(1UL << 2U);
+
+  /* FIFO threshold selection: full FIFO */
+  DMA2_Stream2->FCR |= (uint32_t)(3UL << 0U);
+
+  /* TBD Add PSize */
+
+  /* TBD Enable DMA interrupt trough NVIC */
 
 }
 
--- Src/Mcal/Reg.h
+++ Src/Mcal/Reg.h
@@ -3,6 +3,23 @@
 
   #include <stdint.h>
 
+  /* ----------------------------------------------------------------------------------- */
+  /*  Types definition                                                                   */
+  /* ----------------------------------------------------------------------------------- */
+  typedef struct
+  {
+    uint32_t CR;     /* DMA stream x configuration register      */
+    uint32_t NDTR;   /* DMA stream x number of data register     */
+    uint32_t PAR;    /* DMA stream x peripheral address register */
+    uint32_t M0AR;   /* DMA stream x memory 0 address register   */
+    uint32_t M1AR;   /* DMA stream x memory 1 address register   */
+    uint32_t FCR;    /* DMA stream x FIFO control register       */
+  } DMA_Stream_TypeDef;
+
+  /* ----------------------------------------------------------------------------------- */
+  /* Macros                                                                              */
+  /* ----------------------------------------------------------------------------------- */
+
   /* Base addresses for peripheral registers */
   #define SCB_BASE            0xE000ED00UL
   #define RCC_BASE            0x40023800UL
@@ -11,6 +28,7 @@
   #define GPIOA_BASE          0x40020000UL
   #define SPI_BASE            0x40013000UL
   #define STK_BASE            0xE000E010UL
+  #define DMA2_BASE           0x40026400UL
 
   /* SCB registers */
   #define SCB_CPACR           (*(volatile uint32_t*)(SCB_BASE + 0x88UL))
@@ -36,22 +54,28 @@
   #define FLASH_ACR           (*(volatile uint32_t*)(FLASH_BASE + 0x00UL))
 
   /* GPIOA registers */
-  #define GPIOA_MODER         (*(volatile uint32_t *)(GPIOA_BASE + 0x00UL))
-  #define GPIOA_OTYPER        (*(volatile uint32_t *)(GPIOA_BASE + 0x04UL))
-  #define GPIOA_OSPEEDR       (*(volatile uint32_t *)(GPIOA_BASE + 0x08UL))
-  #define GPIOA_PUPDR         (*(volatile uint32_t *)(GPIOA_BASE + 0x0CUL))
-  #define GPIOA_ODR           (*(volatile uint32_t *)(GPIOA_BASE + 0x14UL))
-  #define GPIOA_AFRL          (*(volatile uint32_t *)(GPIOA_BASE + 0x20UL))
+  #define GPIOA_MODER         (*(volatile uint32_t*)(GPIOA_BASE + 0x00UL))
+  #define GPIOA_OTYPER        (*(volatile uint32_t*)(GPIOA_BASE + 0x04UL))
+  #define GPIOA_OSPEEDR       (*(volatile uint32_t*)(GPIOA_BASE + 0x08UL))
+  #define GPIOA_PUPDR         (*(volatile uint32_t*)(GPIOA_BASE + 0x0CUL))
+  #define GPIOA_ODR           (*(volatile uint32_t*)(GPIOA_BASE + 0x14UL))
+  #define GPIOA_AFRL          (*(volatile uint32_t*)(GPIOA_BASE + 0x20UL))
 
 
   /* SPI1 registers */
-  #define SPI_CR1             (*(volatile uint32_t *)(SPI_BASE + 0x00UL))
-  #define SPI_CR2             (*(volatile uint32_t *)(SPI_BASE + 0x04UL))
-  #define SPI_SR              (*(volatile uint32_t *)(SPI_BASE + 0x08UL))
-  #define SPI_DR              (*(volatile uint32_t *)(SPI_BASE + 0x0CUL))
-  #define SPI_CRCPR           (*(volatile uint32_t *)(SPI_BASE + 0x10UL))
-  #define SPI_RXCRCR          (*(volatile uint32_t *)(SPI_BASE + 0x14UL))
-  #define SPI_TXCRCR          (*(volatile uint32_t *)(SPI_BASE + 0x18UL))
+  #define SPI_CR1             (*(volatile uint32_t*)(SPI_BASE + 0x00UL))
+  #define SPI_CR2             (*(volatile uint32_t*)(SPI_BASE + 0x04UL))
+  #define SPI_SR              (*(volatile uint32_t*)(SPI_BASE + 0x08UL))
+  #define SPI_DR              (*(volatile uint32_t*)(SPI_BASE + 0x0CUL))
+  #define SPI_CRCPR           (*(volatile uint32_t*)(SPI_BASE + 0x10UL))
+  #define SPI_RXCRCR          (*(volatile uint32_t*)(SPI_BASE + 0x14UL))
+  #define SPI_TXCRCR          (*(volatile uint32_t*)(SPI_BASE + 0x18UL))
 
+  /* DMA2 registers */
+  #define DMA2_STREAM2_BASE   (*(volatile uint32_t*)(DMA2_BASE + 0x040UL))
+  #define DMA2_STREAM3_BASE   (*(volatile uint32_t*)(DMA2_BASE + 0x058UL))
+
+  #define DMA2_Stream2        ((volatile DMA_Stream_TypeDef*) DMA2_STREAM2_BASE)
+  #define DMA2_Stream3        ((volatile DMA_Stream_TypeDef*) DMA2_STREAM3_BASE)
 
 #endif /* REG_2023_08_26_H */
